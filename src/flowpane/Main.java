ebb59e81961f09cb6955e201a7c44ffb99d03ed7
= <=>
2020-08-19 11:05:05 +0500

    FlowPane and Button

--- src/flowpane/Main.java
+++ src/flowpane/Main.java
@@ -2,6 +2,9 @@ package flowpane;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -18,13 +21,22 @@ public class Main extends Application {
         Button btn3 = new Button("Another Button");
         btn2.setText("New _Button");
 //        btn2.setMinWidth(50);
-
+        
         //Create FlowPane
         flowPane = new FlowPane(); //0 horizontal gap, 0 vertical gap
         //Add children
         System.out.println(flowPane.getChildren());
         flowPane.getChildren().addAll(btn1,btn2,btn3);
         System.out.println(flowPane.getChildren());
+        //Set the alignment of flowpane
+//        flowPane.setAlignment(Pos.TOP_RIGHT);
+        //Set Horizontal Gap
+        flowPane.setHgap(20);
+        //Vertical Gap
+        flowPane.setVgap(20);
+        flowPane.setPadding(new Insets(20));
+        //Set the orientation
+//        flowPane.setOrientation(Orientation.VERTICAL);
         //Scene
         Scene scene = new Scene(flowPane,200,200);
         //Set the Scene
